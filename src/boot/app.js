2a2fcb9ae196a5802526608922dcd09dc4b11cf9
endropie <hendrowibowo@msn.com>
2019-11-14 13:53:16 +0700

    FIX: Update Boot $app

--- src/boot/app.js
+++ src/boot/app.js
@@ -134,6 +134,17 @@ export default async ({ app, store, router, Vue }) => {
         Notify.create({ ...this.getMode(values, desc), color: 'green-7', icon: 'check_circle' })
       }
     },
+    number_abbreviate (num, fixed) {
+      if (num === null) return null
+      if (num === 0) return '0'
+      fixed = (!fixed || fixed < 0) ? 0 : fixed
+      let b = (num).toPrecision(2).split('e'),
+        k = b.length === 1 ? 0 : Math.floor(Math.min(b[1].slice(1), 14) / 3), // floor at decimals, ceiling at trillions
+        c = k < 1 ? num.toFixed(0 + fixed) : (num / Math.pow(10, k * 3)).toFixed(1 + fixed), // divide by power
+        d = c < 0 ? c : Math.abs(c),
+        e = d + ['', 'K', 'M', 'B', 'T'][k]
+      return e
+    },
     number_format (number, decimals, decimator, separator, isABS = false) {
       var settings = {
         decimals: 2,
