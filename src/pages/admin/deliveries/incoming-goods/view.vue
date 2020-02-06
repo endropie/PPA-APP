f0c7a77c7be037578f12e2ba79dbd67a4b516e89
endropie <hendrowibowo@msn.com>
2020-02-06 09:42:27 +0700

    FIX: update PreDelivery create on IncomingGood.

--- src/pages/admin/deliveries/incoming-goods/view.vue
+++ src/pages/admin/deliveries/incoming-goods/view.vue
@@ -138,7 +138,7 @@
               }
             },
             { label: 'PDO', color:'blue-10', icon: 'assignment',
-              hidden: !this.$app.can('pre-deliveries-create') || rsView.order_mode !== 'NONE',
+              hidden: !this.$app.can('pre-deliveries-create'),
               detail: $tc('form.add_new',1, {v:'PDO'}),
               actions: () => {
                 $router.push(`/admin/deliveries/pre-deliveries/create?incoming_good_id=${ROUTE.params.id}`)
