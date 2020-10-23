0e4ced0c317aeb76489ce015b288f864f19cb8d1
endropie <hendrowibowo@msn.com>
2020-10-23 14:01:41 +0700

    fix(TripDays): custom view.

--- src/pages/admin/deliveries/delivery-trips/trip-days.vue
+++ src/pages/admin/deliveries/delivery-trips/trip-days.vue
@@ -9,13 +9,24 @@
               dense hide-bottom-space hide-dropdown-icon
               standout="bg-blue-grey-5 text-white"
               :debounce="3000"
-              @input="LIST.load"
+              @change="LIST.load"
             />
             <q-space />
+            <ux-select
+              v-model="LIST.filter.customer_id" clearable
+              :label="$tc('general.customer')"
+              dense hide-bottom-space hide-dropdown-icon
+              standout="bg-blue-grey-5 text-white"
+              filter map-options emit-value
+              source="/api/v1/incomes/customers?mode=all&--limit=10"
+              option-value = 'id'
+              option-label = 'name'
+              @input="LIST.load"
+            />
             <q-toggle left-label v-model="isFullTime" label="FULL TIME" />
           </div>
         </q-card-section>
-        <q-markup-table dense flat bordered separator="cell">
+        <q-markup-table dense flat bordered separator="cell" class="table-sticky-column">
           <thead>
             <tr class="head-column">
               <th class="text-left">
@@ -29,89 +40,72 @@
             </tr>
           </thead>
           <tbody v-if="isFullTime">
-            <tr v-for="(cust, index) in LIST.rowdata" :key="index">
+            <tr v-for="(cust, index) in CUSTOMERS" :key="index">
               <td class="text-left">
                 <div class=" q-py-xs">{{cust.code}}</div>
               </td>
               <template v-for="(cTime, iTime) in time24">
                 <td :key="iTime" :row-time-value="cTime" v-show="!HIDE_TIME[iTime]"
-                  :class="{'bg-blue-grey text-white': isTriped(cTime, cust)}"
-                  align="center"
+                  :class="getMiniBoxClass(TASKS, cust, cTime)"
+                  class="no-padding" align="center"
                 >
-                  <span v-text="'D'" v-if="isTriped(cTime, cust)" />
+                  <div v-if="TASKS[cust.id] && TASKS[cust.id][cTime]">
+                    <q-icon size="20px"
+                      :name="getMiniBoxIcon(TASKS[cust.id], cTime)"
+                    />
+                  </div>
                 </td>
               </template>
             </tr>
           </tbody>
           <tbody v-else>
-            <tr v-for="(cust, index) in LIST.rowdata" :key="index">
+            <tr v-for="(cust, index) in CUSTOMERS" :key="index">
               <td class="q-pa-none text-left">
                 <div class=" q-py-xs">{{cust.code}}</div>
               </td>
               <template v-for="(cTime, iTime) in time24">
                 <td :key="iTime" :row-time-value="cTime"
                   v-show="!HIDE_TIME[iTime]"
-                  class="q-py-sm" style="min-width:200px"
+                  class="q-py-sm" style="min-width:180px"
                   :class="{
-                    'bg-grey-10 text-white': $q.dark.isActive && !isTriped(cTime, cust),
-                    'bg-grey-4 text-white': !$q.dark.isActive && !isTriped(cTime, cust),
+                    'bg-grey-10 text-white': $q.dark.isActive && !isTriped(cust, cTime),
+                    'bg-grey-4 text-white': !$q.dark.isActive && !isTriped(cust, cTime),
                   }"
                 >
-                <template v-if="TASK_TIME && TASK_TIME[iTime]" >
-                  <q-chip v-for="(task, iTask) in TASK_TIME[iTime].filter(x => x.customer_id === cust.id)" :key="iTask"
-                    dense square color="blue-grey" text-color="white"
-                    :icon="task.is_loaded ? 'local_shipping' : 'done_all'"
-                    :outline="!isTriped(cTime, cust)"
-                    class="text-weight-medium"
+                <div class="column q-gutter-xs self-start" v-if="TASKS[cust.id] && TASKS[cust.id][cTime]" >
+                  <q-btn v-for="(task, iTask) in TASKS[cust.id][cTime]" :key="iTask" v-show="iTask < 2 || TASKS[cust.id][cTime].length === 3"
+                    dense unelevated size="12px"
+                    :color="getBoxColor(task)"
+                    :text-color="$q.dark.isActive ? 'white' : undefined"
+                    :icon="getBoxIcon(task)"
+                    :outline="!isTriped(cust, cTime)"
                     :label="task.fullnumber"
-                    v-show="iTask === 0"
+                    @click="setBoxlink(task)"
                   />
-                  <q-btn dense color="blue-grey" size="sm" icon="add" unelevated v-if="TASK_TIME[iTime].filter(x => x.customer_id === cust.id).length > 1" >
+                  <q-btn dense outline color="blue-grey" size="12px"
+                    :label="`${TASKS[cust.id][cTime].length - 2}+`"
+                    v-if="TASKS[cust.id][cTime].length > 3"
+                  >
                     <q-menu>
-                      <q-list bordered>
-                        <q-item clickable v-ripple v-for="(task, iTask) in TASK_TIME[iTime].filter(x => x.customer_id === cust.id)" :key="iTask">
+                      <q-list dense bordered>
+                        <q-item v-for="(task, iTask) in TASKS[cust.id][cTime]" :key="iTask" v-show="iTask > 1"
+                          clickable v-ripple
+                          @click="setBoxlink(task)"
+                        >
                           <q-item-section avatar>
-                            <q-icon color="primary" :name="task.is_loaded ? 'local_shipping' : 'done_all'" />
+                            <q-icon :name="getBoxIcon(task)" :color="getBoxColor(task)" />
                           </q-item-section>
-                          <q-item-section>{{task.fullnumber}}</q-item-section>
+                          <q-item-section :class="`text-${getBoxColor(task)}`" >{{task.fullnumber}}</q-item-section>
                         </q-item>
                       </q-list>
                     </q-menu>
                   </q-btn>
-
-                  <q-icon name="warning" color="orange" class="float-right"
-                    v-if="!TASK_TIME[iTime].filter(x => x.customer_id === cust.id).length && isTriped(cTime, cust)"
-                  >
+                </div>
+                <div class="column q-gutter-xs self-start" v-else-if="isTriped(cust, cTime)">
+                  <q-icon name="warning" color="orange" class="self-center" size="15px">
                     <q-tooltip>PDO undefined</q-tooltip>
                   </q-icon>
-                </template>
-                </td>
-              </template>
-            </tr>
-            <tr v-for="(exCust, iEXData) in EXDATA.CUSTOMER" :key="iEXData">
-              <td class="q-pa-none text-left">
-                <div class=" q-py-xs">{{exCust.code}}
                 </div>
-              </td>
-              <template v-for="(cTime, iTime) in time24">
-                <td :key="iTime" :row-time-value="cTime"
-                  v-show="!HIDE_TIME[iTime]"
-                  class="q-py-sm" style="min-width:200px"
-                  :class="{
-                    'bg-grey-10 text-white': $q.dark.isActive,
-                    'bg-grey-4 text-white': !$q.dark.isActive,
-                  }"
-                >
-                <template v-if="EXDATA.TASK[iEXData]" >
-                  <q-chip v-for="(task, iTask) in EXDATA.TASK[iEXData].filter(x => String(x.trip_time).substring(0,2) === cTime)" :key="iTask"
-                    dense square
-                    :label="task.fullnumber"
-                    :icon="task.is_loaded ? 'local_shipping' : 'done_all'"
-                    :outline="true"
-                    color="blue-grey" text-color="white"
-                    class="text-weight-medium"
-                  />
-                </template>
                 </td>
               </template>
             </tr>
@@ -141,8 +135,8 @@ export default {
         },
         rowdata: [],
         rowtask: [],
-        rowload: [],
         filter: {
+          customer_id: null,
           trip_date: this.$app.moment().format('YYYY-MM-DD')
         },
         load: this.load
@@ -153,13 +147,35 @@ export default {
     this.load()
   },
   computed: {
+    TASKS () {
+      const result = { CUSTOMER: {} }
+      const ex = this.LIST.rowtask
+      ex.map((item, i) => {
+        const timecode = String(item.trip_time).substring(0, 2)
+        if (!result[item.customer_id]) result[item.customer_id] = {}
+        if (!result[item.customer_id][timecode]) result[item.customer_id][timecode] = []
+        result[item.customer_id][timecode].push(item)
+      })
+      return result
+    },
+    CUSTOMERS () {
+      let result = []
+      this.LIST.rowdata.map(item => {
+        if (!result.find(x => x.id === item.id)) result.push(item)
+      })
+      this.LIST.rowtask.map(item => {
+        if (!result.find(x => x.id === item.customer_id)) result.push(item.customer)
+      })
+      return result
+    },
     EXDATA () {
       const result = { CUSTOMER: {}, TASK: {} }
       const ex = this.LIST.rowtask.filter(x => !this.LIST.rowdata.find(cust => cust.id === x.customer_id))
-      ex.every((item, i) => {
+      ex.map((item, i) => {
         if (!result.CUSTOMER[item.customer_id]) result.CUSTOMER[item.customer_id] = item.customer
-        if (!result.TASK[item.customer_id]) result.TASK[item.customer_id] = []
-        result.TASK[item.customer_id].push(item)
+        if (!result.TASK[item.customer_id]) result.TASK[item.customer_id] = {}
+        if (!result.TASK[item.customer_id][String(item.trip_time).substring(0, 2)]) result.TASK[item.customer_id][String(item.trip_time).substring(0, 2)] = []
+        result.TASK[item.customer_id][String(item.trip_time).substring(0, 2)].push(item)
       })
       return result
     },
@@ -194,43 +210,62 @@ export default {
     },
     isCanDelete () {
       return this.$app.can('items-delete')
-    },
-    CustomerOptions () {
-      return (this.SHEET.customers.data.map(item => ({ label: `${item.code} - ${item.name}`, value: item.id, rowdata: item })) || [])
     }
   },
   methods: {
-    isTriped (time, cust) {
+    getBoxIcon (task) {
+      if (task.is_checkout) return 'local_shipping'
+      if (task.is_loaded) return 'move_to_inbox'
+      return 'bookmark'
+    },
+    getBoxColor (task) {
+      if (task.is_checkout) return 'green-10'
+      if (task.is_loaded) return 'indigo'
+      return 'blue-grey'
+    },
+    getMiniBoxIcon (tasktime, time) {
+      if (!tasktime[time]) return 'clear'
+      const trips = tasktime[time]
+      if (trips.find(x => x.is_checkout)) return 'local_shipping'
+      if (trips.find(x => x.is_loaded)) return 'move_to_inbox'
+      if (trips.length) return 'bookmark'
+      return 'clear'
+    },
+    getMiniBoxClass (task, cust, time) {
+      if (!task[cust.id] || !task[cust.id][time]) return ''
+      const trips = task[cust.id][time]
+      const background = Boolean(cust.customer_trips && cust.customer_trips.find(x => String(x.time).substring(0, 2) === time))
+
+      if (trips.find(x => x.is_checkout)) return `${background ? 'bg-green-10 text-white' : 'bg-grey-4 text-green-10'}`
+      if (trips.find(x => x.is_loaded)) return `${background ? 'bg-blue-8 text-white' : 'bg-grey-4 text-blue-8'}`
+      return `${background ? 'bg-blue-grey text-white' : 'bg-grey-4 text-blue-grey'}`
+    },
+    isTriped (cust, time) {
+      if (!cust.customer_trips) return false
       return cust.customer_trips.find(x => String(x.time).substring(0, 2) === time && x.intday === this.INTDAY)
     },
-    getLoad () {
-      let url = `${this.LIST.resource.delivery_load}?mode=all&date=${this.LIST.filter.trip_date}`
-      this.$q.loading.show()
-      this.$axios.get(url)
-        .then((response) => {
-          this.LIST.rowload = response.data
-        }).catch((error) => {
-          this.$app.response.error(error.response || error)
-        }).finally(() => {
-          this.$q.loading.hide()
-        })
+    setBoxlink (task) {
+      this.$router.push(`/admin/deliveries/delivery-tasks/${task.id}`)
     },
     getTask () {
-      let url = `${this.LIST.resource.delivery_task}?mode=all&--with=customer&date=${this.LIST.filter.trip_date}`
-      this.$q.loading.show()
+      let customer = this.LIST.filter.customer_id ? `&customer_id=${this.LIST.filter.customer_id}` : ''
+      let url = `${this.LIST.resource.delivery_task}?mode=all&sort=created_at&--with=customer&date=${this.LIST.filter.trip_date}${customer}`
+      // console.warn('GET', url)
       this.$axios.get(url)
         .then((response) => {
           this.LIST.rowtask = response.data
         }).catch((error) => {
           this.$app.response.error(error.response || error)
         }).finally(() => {
-          this.$q.loading.hide()
+          // code..
         })
     },
     getTrip (done = null) {
+      let customer = this.LIST.filter.customer_id ? `&id=${this.LIST.filter.customer_id}` : ''
       const intday = this.$app.moment(this.LIST.filter.trip_date).day()
-      let url = `${this.LIST.resource.api}?mode=all&trip_intday=${intday}&--with=customer_trips`
+      let url = `${this.LIST.resource.api}?mode=all&--with=customer_trips&trip_intday=${intday}${customer}`
       this.$q.loading.show()
+      // console.warn('GET', url)
       this.$axios.get(url)
         .then((response) => {
           this.LIST.rowdata = response.data
@@ -245,7 +280,6 @@ export default {
       if (!this.LIST.filter.trip_date) return console.warn('TRIP UNDEFINED!!!')
       this.getTrip(done)
       this.getTask()
-      this.getLoad()
     }
   }
 }
