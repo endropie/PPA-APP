00f38b4511a56e87aa67aa027a24430ba7565fc3
endropie <hendrowibowo@msn.com>
2021-07-15 15:45:16 +0700

    fixed: v-if is_internal in right header delivery-order

--- src/pages/admin/deliveries/delivery-orders/view.vue
+++ src/pages/admin/deliveries/delivery-orders/view.vue
@@ -61,7 +61,7 @@
             <ux-chip-status :row="rsView" tag outline dense square icon='bookmark' class="no-margin" />
           </div>
           <div class="text-subtitle2 text-weight-bold text-uppercase text-center on-right">
-            <span v-if="page.data.is_internal">{{ $tc('general.sj_internal', 2) }} </span>
+            <span v-if="rsView.is_internal">{{ $tc('general.sj_internal', 2) }} </span>
             <span v-else>{{ $tc('general.sj_delivery',2) }} {{ page.data.transaction }}</span>
           </div>
         </div>
